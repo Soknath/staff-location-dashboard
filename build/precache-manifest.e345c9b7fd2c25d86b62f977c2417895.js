dabaf0114a0750fb8542fcf566851e54072a979e
MIL Soknath <milsoknath@MILs-MacBook-Pro.local>
2020-04-30 01:14:11 +0700

    add build

---
+++ build/precache-manifest.e345c9b7fd2c25d86b62f977c2417895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b418dd922c5c239456d0f173b28fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6899cb0edaa63eaa30",
+    "url": "/static/css/2.75edc52f.chunk.css"
+  },
+  {
+    "revision": "9542e61aa8c9d7716e0b",
+    "url": "/static/css/main.8fe74ac3.chunk.css"
+  },
+  {
+    "revision": "2c6899cb0edaa63eaa30",
+    "url": "/static/js/2.4ca4de78.chunk.js"
+  },
+  {
+    "revision": "b5d23eb95f12f07608aa002de8d8de17",
+    "url": "/static/js/2.4ca4de78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9542e61aa8c9d7716e0b",
+    "url": "/static/js/main.778f9fa9.chunk.js"
+  },
+  {
+    "revision": "7f6e11ce90d8957e785c",
+    "url": "/static/js/runtime-main.3abeeb2e.js"
+  }
+]);
